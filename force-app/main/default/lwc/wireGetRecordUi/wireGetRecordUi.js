11265ceaf5b78480c71ada9db2892ae8c46bc527
Manas Datta <manasdatta.cs@gmail.com>
2021-07-27 20:59:07 +0530

    commit 2

---
+++ force-app/main/default/lwc/wireGetRecordUi/wireGetRecordUi.js
@@ -0,0 +1,31 @@
+import { LightningElement,wire,api } from 'lwc';
+import {getRecordUi} from 'lightning/uiRecordApi'
+export default class WireGetRecordUi extends LightningElement {
+    formFields=[
+        {"fieldName":"AccountNumber", "label":"Account Number"},
+        {"fieldName":"AnnualRevenue", "label":"Annual Revenue"},
+        {"fieldName":"Name", "label":"Name"},
+        {"fieldName":"Phone", "label":"Phone"}
+    ]
+
+    @api recordId
+    @wire(getRecordUi,{recordIds:'$recordId',layoutTypes:'Full',modes:'Edit'})
+    accountRecordUiHandler(data,error){
+        if(data){
+            console.log(data)
+            this.formFields = this.formFields.map(item=>{
+                return {...item, value:data.records[this.recordId].fields[item.fieldName].value}
+            })
+
+        }
+        if(error){
+            console.log(error)
+
+        }
+
+    }
+
+
+
+
+}
